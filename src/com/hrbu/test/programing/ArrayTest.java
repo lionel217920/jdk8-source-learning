7528bdf4011b8f138c8cfaadfec7add778817bd6
Lionel <lionel217920@163.com>
2021-01-25 21:07:19 +0800

    add dataType test

---
+++ src/com/hrbu/test/programing/ArrayTest.java
@@ -0,0 +1,8 @@
+package com.hrbu.test.programing;
+
+public class ArrayTest {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/hrbu/test/programing/DataTypeTest.java
@@ -0,0 +1,19 @@
+package com.hrbu.test.programing;
+
+public class DataTypeTest {
+
+    public static void main(String[] args) {
+        double a = 2.0;
+        double b = 1.1;
+        System.out.print(a - b);
+    }
+
+    public void testFloat() {
+        float a = 3.14f;
+        short b = 1231;
+
+        if (Double.isNaN(b)) {
+
+        }
+    }
+}
